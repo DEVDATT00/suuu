ee2768ac510cafdca918c0a1ddb80614a695d225
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-07-18 14:28:36 +0530

    Time: 285 ms (59.4%), Space: 301.8 MB (53.4%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,40 +1,29 @@
 class Solution {
-public:
-    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-
-        vector<vector<int>> graph(n);
-
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-        }
-
-        vector<bool> visited(n, false);
-        queue<int> q;
-
-        q.push(source);
-        visited[source] = true;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            if (node == destination)
-                return true;
-
-            for (int i = 0; i < graph[node].size(); i++) {
-                int neighbour = graph[node][i];
-
-                if (!visited[neighbour]) {
-                    visited[neighbour] = true;
-                    q.push(neighbour);
+    bool check(vector<bool>& visited, vector<vector<int>>& graph,int source, int destination) {
+        if (source == destination)
+            return true;
+        visited[source] = false;
+        int tem;
+        for(int i = 0 ; i < graph[source].size() ; i++){
+            tem = graph[source][i];
+            if(visited[tem]){
+                if(check(visited, graph, tem, destination)){
+                    return true;
                 }
             }
         }
-
         return false;
+    }
+
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source,int destination) {
+        vector<vector<int>> graph(n);
+        int u, v;
+        for (int i = 0; i < edges.size(); i++) {
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }
+        vector<bool> visited(n, true);
+        return check(visited, graph, source, destination);
     }
 };
