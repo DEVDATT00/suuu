e88cd587ea98977d8c29f0a539b76b846914c9e3
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-07-19 10:19:40 +0530

    Time: 0 ms (100%), Space: 14.4 MB (72.58%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,20 +1,17 @@
 class Solution {
-    void justdothis(vector<vector<int>>&rooms,vector<bool>&check,set<int>&s,int t){
+    void justdothis(vector<vector<int>>&rooms,vector<bool>&check,int t){
         if(check[t]){
             return;
         }
         check[t] = true;
         for(int i = 0 ; i < rooms[t].size() ; i++){
-            s.insert(rooms[t][i]);
-            justdothis(rooms,check,s,rooms[t][i]);
+            justdothis(rooms,check,rooms[t][i]);
         }
     }
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        set<int>s;
-        s.insert(0);
         vector<bool>check(rooms.size(),false);
-        justdothis(rooms,check,s,0);
+        justdothis(rooms,check,0);
         for(int i = 0 ; i < check.size() ; i++){
             if(!check[i]){
                 return false;
