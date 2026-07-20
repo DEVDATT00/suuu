7e4d7b780e50c27db7d164b16073805776353fd1
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-07-20 09:16:49 +0530

    Time: 1 ms (77.89%), Space: 102.1 MB (45.29%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,32 +1,23 @@
 class Solution {
-    int maintask(vector<vector<bool>>& check, vector<vector<int>>& grid, int i,int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size()) {
-            return 1;
-        }
-        if (grid[i][j] == 0 ) {
-            return 1;
-        }
-        if(!check[i][j]){
-            return 0;
-        }
-        check[i][j] = false;
-        return maintask(check, grid, i - 1, j)
-            +maintask(check, grid, i + 1, j)
-            +maintask(check, grid, i, j - 1)
-            +maintask(check, grid, i, j + 1);
-    }
-
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int tem;
-        vector<vector<bool>> check(grid.size(),vector<bool>(grid[0].size(), true));
+        int perimeter = 0;
+
         for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j]) {
-                    return maintask(check, grid, i, j);
+            for (int j = 0; j < grid[0].size(); j++) {
+
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+
+                    if (i > 0 && grid[i - 1][j] == 1)
+                        perimeter -= 2;
+
+                    if (j > 0 && grid[i][j - 1] == 1)
+                        perimeter -= 2;
                 }
             }
         }
-        return 0;
+
+        return perimeter;
     }
 };
