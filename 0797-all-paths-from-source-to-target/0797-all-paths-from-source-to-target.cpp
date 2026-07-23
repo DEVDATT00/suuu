338e49be461ff568a2a3b5b7a831ff723407408e
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-07-23 21:26:34 +0530

    Time: 13 ms (22.86%), Space: 19.2 MB (21.78%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    void t(vector<vector<int>>& ans, vector<int> tem,vector<vector<int>>& graph, int target, int start) {
+        tem.push_back(start);
+        if (start == target) {
+            ans.push_back(tem);
+            return;
+        }
+        for (int i = 0; i < graph[start].size(); i++) {
+            t(ans, tem, graph, target, graph[start][i]);
+        }
+    }
+
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> tem;
+        vector<vector<int>> ans;
+        t(ans, tem, graph, graph.size() - 1, 0);
+        return ans;
+    }
+};
