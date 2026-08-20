b92d4d387e938410e756027864733655e06de3f6
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-08-20 10:16:33 +0530

    Time: 3 ms (93.93%), Space: 25.4 MB (80.72%) - LeetHub

---
+++ 0817-linked-list-components/0817-linked-list-components.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        int count = 0;
+        while (head != nullptr) {
+            if (st.count(head->val) && (head->next == nullptr || !st.count(head->next->val))) {
+                count++;
+            }
+            head = head->next;
+        }
+        return count;
+    }
+};
