8c73c72b5be0036f43c7f846845aea7916f1e341
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-08-25 12:31:09 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.7%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int digitFrequencyScore(int n) {
-        vector<int>count(10,0);
-        while(n > 0){
-            count[n % 10]++;
+        int sum = 0;
+        while(n){
+            sum += n % 10;
             n /= 10;
         }
-        int ans = 0;
-        for(int i = 0 ; i < 10 ; i++){
-            ans += count[i] * i;
-        }
-        return ans;
+        return sum;
     }
 };
