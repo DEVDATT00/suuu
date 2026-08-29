736f09f51b1bbc14a9f0d273af26400de9f94e28
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-08-29 20:53:30 +0530

    Time: 3 ms (36.92%), Space: 8.7 MB (67.59%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int countrows = 0;
-        for (int i = 1 ; i <= n; i++ ) {
+        int comp = 0;
+
+        for (int i = 1; i<=n; i++) {
             n -= i;
-            countrows++;
+            comp++;
         }
-        return countrows ;
+
+        return comp;
     }
 };
