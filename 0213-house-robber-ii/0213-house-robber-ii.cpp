82287f6823fa6be5675bafabd647a52f70d97deb
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-09-12 17:31:29 +0530

    Time: 0 ms (100%), Space: 10.9 MB (37.94%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int justdo(vector<int>& nums, int i, int end , vector<int>&dp) {
+        if (i > end)
+            return 0;
+        if(dp[i] != -1)
+            return dp[i];
+        int rob = nums[i] + justdo(nums, i + 2, end,dp);
+        int skip = justdo(nums, i + 1, end,dp);
+        return dp[i] = max(rob, skip);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        vector<int>dp1(n,-1);
+        vector<int>dp2(n,-1);
+        int case1 = justdo(nums, 0, n - 2,dp1);
+        int case2 = justdo(nums, 1, n - 1,dp2);
+        return max(case1, case2);
+    }
+};
