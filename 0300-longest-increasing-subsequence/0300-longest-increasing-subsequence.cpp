e6fc63633349ec6ef74586c59429385b269ffd89
Devdatt m patel <185817338+DEVDATT00@users.noreply.github.com>
2026-09-16 07:40:00 +0530

    Time: 67 ms (57.92%), Space: 14.3 MB (71.03%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,17 @@
 class Solution {
-    int justdoit(vector<int>& nums, int index, int previous, vector<vector<int>>& dp) {
-        if (index == nums.size())
-            return 0;
-        if (dp[index][previous + 1] != -1)
-            return dp[index][previous + 1];
-        int skip = justdoit(nums, index + 1, previous, dp);
-        int take = 0;
-        if (previous == -1 || nums[previous] < nums[index]) {
-            take = 1 + justdoit(nums, index + 1, index, dp);
-        }
-        return dp[index][previous + 1] = max(take, skip);
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return justdoit(nums, 0, -1, dp);
+        vector<int> dp(n, 1);
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
     }
 };
